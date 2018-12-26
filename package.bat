6e09ba8414bf800cacc113d4622b9ce24134aa50
Ziegler <18589@icf.com>
2018-12-26 11:33:24 -0500

    Added GH updates to package

--- package.bat
+++ package.bat
@@ -1,12 +1,15 @@
 set function_name=aws-code-index-stream-bulk-load
-
+title %function_name%
 
 call cd .\tests
-call run_integration_tests.bat
+call run_all_tests.bat
 set test_error=%ERRORLEVEL%
 call cd ..
-IF %test_error%==1 goto failed
+IF %test_error%==1 goto failed_tests
 
+call commit_and_push.bat
+set test_error=%ERRORLEVEL%
+IF %test_error%==1 goto failed_commit
 
 
 REM Zip the lambda function
@@ -20,10 +23,14 @@ REM Upload the new code
 call aws lambda update-function-code --function-name %function_name% --zip-file fileb://lambda_function.zip
 goto end
 
-:failed
+:failed_tests
 echo *** Tests failed, so aborted packaging
+goto end
 
+
+:failed_commit
+echo *** Commit failed, so aborted packaging
+goto end
 
 :end
 echo %time%
-
